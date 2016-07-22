d9e6b81bfa0e46438a6b950eb00e93b189f994fa
wab <contact@wabdesign.fr>
2016-07-22 15:42:05 +0200

    utilisation de la methode .fetch() au lieu du module vue-ressource

--- src/components/City.vue
+++ src/components/City.vue
@@ -92,27 +92,39 @@ export default {
   },
   methods: {
     getData: function (city) {
-      this.$http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',&lang=fr&units=metric&appid=801c6f79c0eaf3711dbda7eb7f2676c6').then((response) => {
-        // success callback
-        this.data = response.data
-        this.LatLng = {
-          lat: this.data.coord.lat,
-          lng: this.data.coord.lon
-        }
-        this.weather = this.data.weather[0]
-        this.main = this.data.main
-        this.cityname = this.data.name
-        this.wind = this.data.wind
-      }, (response) => {
-          // error callback
-        console.log(response)
-      })
+      // Change la requête en fonction de la ville
+      let query = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&units=metric&appid=801c6f79c0eaf3711dbda7eb7f2676c6`
+
+      // Récupère les données
+      let datasPromise = window.fetch(query)
+
+      datasPromise
+        // Lorsque les données sont récupérees les mettre en json
+        .then(data => data.json())
+        // Lorsque les données sont en json, les dispatcher dans this.data
+        .then(data => {
+          this.data = data
+          this.LatLng = {
+            lat: this.data.coord.lat,
+            lng: this.data.coord.lon
+          }
+          this.weather = this.data.weather[0]
+          this.main = this.data.main
+          this.cityname = this.data.name
+          this.wind = this.data.wind
+        })
+        // Sinon afficher err
+        .catch((err) => {
+          console.error(err)
+        })
     }
   },
   events: {
+    // Au chargement
     'hook:created': function () {
       this.getData('Rennes')
     },
+    // Mise à jour des données
     updateCity: function (city) {
       this.getData(city)
     }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-import vueResource from 'vue-resource'
 
 import './main.scss'
 
 import App from './App'
-
-Vue.use(vueResource)
 
 /* eslint-disable no-new */
 new Vue({
